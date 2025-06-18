af9f70c14db34b990958fd4b4da7a157303dc560
Joao Peres <jp.peres@campus.fct.unl.pt>
2025-06-19 00:55:33 +0100

    style: improve create expense form UI with shadcn components

--- frontend/src/components/AddExpenseForm.tsx
+++ frontend/src/components/AddExpenseForm.tsx
@@ -2,6 +2,10 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { expenseSchema } from "../schemas/expenseSchema ";
 import { useCreateExpense } from "../hooks/useCreateExpense";
+import { Button } from "./ui/button";
+import { Loader2Icon } from 'lucide-react'
+import { Input } from "./ui/input";
+import { Label } from "@radix-ui/react-label";
 
 const AddExpenseForm = () => {
 
@@ -9,52 +13,47 @@ const AddExpenseForm = () => {
     resolver: zodResolver(expenseSchema)
   });
 
-  const options = Object.entries(expenseSchema.shape.Type.enum).map((op) => {
-
-        return ({
-            value: op[0]
-        })
-    } 
-    );
-    
   const { mutate: createExpense, isPending } = useCreateExpense();
 
   return (
     <>
         <form 
-        className="flex border-2 border-white flex-col gap-y-6 p-6 bg-(image:--color-grad-2) rounded-4xl min-w-[400px]"
+        className="flex border-2 border-white flex-col gap-y-2 p-6 bg-(image:--color-grad-2) rounded-4xl min-w-[400px]"
             onSubmit={handleSubmit((data) => {
                 createExpense(data);
+                reset();
             })}
         >
             <h2 className="text-white italic text-center">Create Expense</h2>
-            <p>Expense Name:</p>
-            <input className="bg-white rounded-2xl px-2" {...register("Name")} type="text" placeholder="Enter the name of the expense"/>
+            <Label htmlFor="Name">Name:</Label>
+            <Input id="Name" className="bg-white rounded-2xl px-2" {...register("Name")} type="text" placeholder="Enter the name of the expense" />
             <p>{errors.Name?.message}</p>
-            <p>Description:</p>
-            <input className="bg-white rounded-2xl px-2" {...register("Description")} type="text" placeholder="Enter the expense description"/>
+            <Label htmlFor="Description">Description:</Label>
+            <Input id="Description" className="bg-white rounded-2xl px-2" {...register("Description")} type="text" placeholder="Enter the expense description"/>
             <p>{errors.Description?.message}</p>
-            <p>Expense Type:</p>
-            <select className="bg-white rounded-2xl px-2" {...register("Type")}>
-                {options.map(op => (
-                    <option value={op.value}>
-                        {op.value}
+            <Label htmlFor="ExpenseType">Expense Type:</Label>
+            <select id="ExpenseType" className="bg-white rounded-2xl px-2" {...register("Type")}>
+                {expenseSchema.shape.Type.options.map(op => (
+                    <option key={op} value={op}>
+                        {op}
                     </option>
                 ))}
             </select>
             <p>{errors.Type?.message}</p>
-            <p>Denomination:</p>
-            <input className="bg-white rounded-2xl px-2" {...register("Denomination", {valueAsNumber: true})} type="number" placeholder="Enter the expense monetary amount" />
+            <Label htmlFor="Denomination">Denomination:</Label>
+            <Input id="Denomination" className="bg-white rounded-2xl px-2" {...register("Denomination", {valueAsNumber: true})} type="number" placeholder="Enter the expense monetary amount" />
             <p>{errors.Denomination?.message}</p>
-            <p>Quantity:</p>
-            <input className="bg-white rounded-2xl px-2" {...register("Quantity", {valueAsNumber: true})} type="number" placeholder="Quantity" />
+            <Label htmlFor="Quantity">Quantity:</Label>
+            <Input id="Quantity" className="bg-white rounded-2xl px-2" {...register("Quantity", {valueAsNumber: true})} type="number" placeholder="Quantity" />
             <p>{errors.Quantity?.message}</p>
-            <p>Date:</p>
-            <input className="bg-white rounded-2xl px-2" {...register("ExpenseDate")} type="date" placeholder="Enter the date of the expense" />
+            <Label htmlFor="ExpenseDate">Expense Date:</Label>
+            <Input className="bg-white rounded-2xl px-2" {...register("ExpenseDate")} type="date" placeholder="Enter the date of the expense" />
             <p>{errors.ExpenseDate?.message}</p>
-            <button className="bg-(image:--color-grad-1) rounded-2xl hover:bg-(image:--color-grad-4) hover:cursor-pointer" disabled={isPending} type="submit">
+            
+            <Button className="bg-(image:--color-grad-1) rounded-2xl hover:bg-(image:--color-grad-4) hover:cursor-pointer disabled:bg-gray-400" disabled={isPending} type="submit">
+                {isPending && <Loader2Icon className="animate-spin" />}
                 {isPending ? 'Submitting...' : 'Submit'}
-            </button>
+            </Button>
         </form>
     </>
   )
